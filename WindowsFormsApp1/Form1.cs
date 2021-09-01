50e0b87a3611b63aaf7921a1c0fadaa884733ce3
U008143 <U008143@XPNOTE-20001114>
2021-09-01 13:50:17 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DesenheAlgo();
+
+        }
+        private void DesenheAlgo()
+        {
+            using (var pincelAzul = new SolidBrush(Color.Blue))
+            {
+                using (var grafico = CreateGraphics())
+                {
+                    grafico.FillRectangle(pincelAzul, new Rectangle(10, 10, 100, 100));
+                }
+            }
+        }
+    }
+}
